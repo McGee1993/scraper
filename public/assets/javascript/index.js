3101e415b19c623b967d92a72b62e1a9ed817186
Mark McGee <mcgee11161993@gmail.com>
2019-10-28 21:09:24 -0400

    scraper app

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,81 @@
+console.log("index.js is connected")
+
+$(document).on("click", "#scrape-button", function() {
+    $.ajax({
+      method: "GET",
+      url: "/scrape"
+    })
+    window.location.replace("/scrape");
+  });
+  
+  //Delete an article
+  $(document).on("click", ".delete-article", function() {
+      var thisId = $(this).attr("data-id");
+      $.ajax({
+        method: "DELETE",
+        url: "/saved/" + thisId
+      })
+      .then(function(data) {
+        console.log(data);
+        location.reload();
+      });
+  });
+  
+  //Save an article
+  $(document).on("click", ".save-article", function() {
+    var thisId = $(this).attr("data-id");
+    $(this).hide();
+    var data = {}
+    data.title =  $("#title-" + thisId).text();
+    data.link = $("#link-" + thisId).text();
+    data.excerpt = $("#excerpt-" + thisId).text();
+    $.ajax({
+      method: "POST",
+      dataType: "json",
+      url: "/api/saved",
+      data: data
+    })
+  });
+  
+  //Go to the notes page for a particular article
+  $(document).on("click", ".note-comment", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId
+    })
+    window.location.replace("/articles/" + thisId);
+  });
+  
+    // Submit a note
+    $(document).on("click", "#submit-note", function() {
+      var thisId = $(this).attr("data-id");
+      $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+          title: $("#title-note").val(),
+          body: $("#note-description").val()
+        }
+      })
+        .then(function(data) {
+          console.log(data);
+          window.location.replace("/articles/" + data._id);
+        });  
+        
+        $("#title-note").val("");
+        $("#note-description").val("");
+    });
+    
+    //delete a note
+    $(document).on("click", ".delete-note", function() {
+      var thisId = $(this).attr("data-id");
+      $.ajax({
+        method: "DELETE",
+        url: "/articles/" + thisId
+      })
+      .then(function(data) {
+        console.log(data);
+        location.reload();
+      }); 
+  });
